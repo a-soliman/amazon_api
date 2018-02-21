395f62155ecf97fc22b6a8654135c115c950b42c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-21 08:05:09 -0800

    coded the api fetch function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var details = require('./details');
+const {OperationHelper} = require('apac');
+
+
+var opHelper = new OperationHelper({
+    awsId:     details.AccessId,
+    awsSecret: details.Secret,
+    assocId:   details.Tag,
+    locale:    'US'
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+
+  	opHelper.execute('ItemSearch', {
+		'SearchIndex': 'Electronics',
+	  	'Keywords': 'surface',
+	  	'ResponseGroup': 'ItemAttributes,Offers'
+	}).then((response) => {
+		res.send(response.result)
+	    //console.log("Results object: ", response.result);
+	    //console.log("Raw response body: ", response.responseBody);
+	}).catch((err) => {
+	    console.error("Something went wrong! ", err);
+	});
 });
 
 module.exports = router;
